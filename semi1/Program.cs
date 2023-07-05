ece93bf7b232347ffbc3e0e8541efab324d648a6
Urmatbek <urmat0791@bk.ru>
2023-07-05 23:06:21 +0600

    prakika

--- semi1/Program.cs
+++ semi1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine()); // console.ReadLine() считывает с консоли
+int result = number * number; // храним результат вычисления число само на себя
+// Console.WriteLine(number);
+Console.WriteLine("ведите число"); // подсказка для пользователя
+Console.WriteLine("квадрат числа =" + result);  // Конкатенация(склеивание) строки через
